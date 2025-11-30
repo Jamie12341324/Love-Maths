0f355059d027cc6ed0ed4874e2b5abb2d5bf1645
Jamie12341324 <peacfulmarinecat@gmail.com>
2025-11-30 19:23:19 +0000

    set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+function runGame(){
+
+}
+function checkAnswer(){
+
+}
+function calculateCorrectAnswer(){
+
+}
+function incrementScore(){
+
+}
+function incrementwrongAnswer(){
+
+}
+function displayAdditionQuestion(){
+
+}
+function displaySubtractQuestion(){
+    
+}
+function displayMultiplyQuestion(){
+    
+}
+function displayAdditionQuestion(){
+    
+}
